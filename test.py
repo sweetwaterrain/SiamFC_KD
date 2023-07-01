c5af3baeba2384f17933f8fdc914a51ecd3640ab
gly <993214226@qq.com>
2023-07-01 19:19:23 +0800

    修改test

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ from siamfc import TrackerSiamFC
 
 
 if __name__ == '__main__':
-    net_path = 'pretrained/siamfc_alexnet_e50.pth'
+    net_path = '/kaggle/input/otb2015/OTB100/siamfc_alexnet_e10.pth'
     tracker = TrackerSiamFC(net_path=net_path)
 
-    root_dir = os.path.expanduser('~/data/OTB')
+    root_dir = os.path.expanduser('/kaggle/input/otb2015/OTB100')
     e = ExperimentOTB(root_dir, version=2015)
     e.run(tracker)
     e.report([tracker.name])
