af2f45643e20736b315cf009ca499dca25cbb005
gly <993214226@qq.com>
2023-07-01 21:16:15 +0800

    11

--- siamfc/siamfc.py
+++ siamfc/siamfc.py
@@ -20,10 +20,8 @@ from .heads import SiamFC
 from .losses import BalancedLoss
 from .datasets import Pair
 from .transforms import SiamFCTransforms
-from .VanillaNet.models.vanillanet import vanillanet_5
 
 __all__ = ['TrackerSiamFC']
-
 
 
 class Net(nn.Module):
@@ -50,14 +48,10 @@ class TrackerSiamFC(Tracker):
         self.device = torch.device('cuda:0' if self.cuda else 'cpu')
 
         # setup model
-        # self.net = Net(
-        #     backbone=AlexNetV1(),
-        #     head=SiamFC(self.cfg.out_scale))
         self.net = Net(
-            backbone=vanillanet_5(),
-            # 修改网络结构，使得输出为4维，和AlexNetV1网络的输出一致
+            backbone=AlexNetV1(),
             head=SiamFC(self.cfg.out_scale))
-        # ops.init_weights(self.net)
+        ops.init_weights(self.net)
         
         # load checkpoint if provided
         if net_path is not None:
@@ -342,3 +336,176 @@ class TrackerSiamFC(Tracker):
         self.labels = torch.from_numpy(labels).to(self.device).float()
         
         return self.labels
+    # 添加知识蒸馏的通用训练代码，教师模型可以是任意模型，学生模型是SiamFC
+    def KD_train(self, teacher_model, seqs, val_seqs=None,):
+        # set to train mode
+        self.net.train()
+        teacher_model.eval()
+        # setup dataset
+        transforms = SiamFCTransforms(
+            exemplar_sz=self.cfg.exemplar_sz,
+            instance_sz=self.cfg.instance_sz,
+            context=self.cfg.context)
+        dataset = Pair(
+            seqs=seqs,
+            transforms=transforms)
+        
+        # setup dataloader
+        dataloader = DataLoader(
+            dataset,
+            batch_size=self.cfg.batch_size,
+            shuffle=True,
+            num_workers=self.cfg.num_workers,
+            pin_memory=self.cuda,
+            drop_last=True)
+        
+        # loop over epochs
+        for epoch in range(self.cfg.epoch_num):
+            # update lr at each epoch
+            self.lr_scheduler.step(epoch=epoch)
+
+            # loop over dataloader
+            for it, batch in enumerate(dataloader):
+                loss = self.KD_train_step(batch, teacher_model, backward=True)
+                print('Epoch: {} [{}/{}] Loss: {:.5f}'.format(
+                    epoch + 1, it + 1, len(dataloader), loss))
+                sys.stdout.flush()
+            
+            # save checkpoint
+            if not os.path.exists('pretrained'):
+                os.makedirs('pretrained')
+            net_path = os.path.join(
+                'pretrained', 'siamfc_alexnet_e%d.pth' % (epoch + 1))
+            torch.save(self.net.state_dict(), net_path)
+
+    @torch.enable_grad()
+    def KD_train_step(self, batch, teacher_model, backward=True):
+        # set network mode
+        self.net.train(backward)
+
+        # parse batch data
+        z = batch[0].to(self.device, non_blocking=self.cuda)
+        x = batch[1].to(self.device, non_blocking=self.cuda)
+        # teacher_model.eval()
+        with torch.set_grad_enabled(backward):
+            # inference
+            responses = self.net(z, x)
+            teacher_responses = teacher_model(z, x)
+            # calculate loss
+            labels = self._create_labels(responses.size())
+            loss = self.criterion(responses, labels)
+            # 添加知识蒸馏的损失
+            loss_KD = self.criterion(responses, teacher_responses)
+            loss = loss + loss_KD
+            if backward:
+                # back propagation
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+        
+        return loss.item()
+    
+    def KD_track(self, img_files, box, teacher_model, visualize=False):
+        frame_num = len(img_files)
+        boxes = np.zeros((frame_num, 4))
+        boxes[0] = box
+        times = np.zeros(frame_num)
+
+        for f, img_file in enumerate(img_files):
+            img = ops.read_image(img_file)
+
+            begin = time.time()
+            if f == 0:
+                self.init(img, box)
+            else:
+                boxes[f, :] = self.KD_update(img, teacher_model)
+            times[f] = time.time() - begin
+
+            if visualize:
+                ops.show_image(img, boxes[f, :])
+
+        return boxes, times
+    
+    @torch.no_grad()
+    def KD_update(self, img, teacher_model):
+        # set to evaluation mode
+        self.net.eval()
+
+        # search images
+        x = [ops.crop_and_resize(
+            img, self.center, self.x_sz * f,
+            out_size=self.cfg.instance_sz,
+            border_value=self.avg_color) for f in self.scale_factors]
+        x = np.stack(x, axis=0)
+        x = torch.from_numpy(x).to(
+            self.device).permute(0, 3, 1, 2).float()
+        
+        # responses
+        x = self.net.backbone(x)
+        responses = self.net.head(self.kernel, x)
+        teacher_responses = teacher_model.head(self.kernel, x)
+        responses = responses.squeeze(1).cpu().numpy()
+        teacher_responses = teacher_responses.squeeze(1).cpu().numpy()
+        # upsample responses and penalize scale changes
+        responses = np.stack([cv2.resize(
+            u, (self.upscale_sz, self.upscale_sz),
+            interpolation=cv2.INTER_CUBIC)
+            for u in responses])
+        teacher_responses = np.stack([cv2.resize(
+            u, (self.upscale_sz, self.upscale_sz),
+            interpolation=cv2.INTER_CUBIC)
+            for u in teacher_responses])
+        responses[:self.cfg.scale_num // 2] *= self.cfg.scale_penalty
+        responses[self.cfg.scale_num // 2 + 1:] *= self.cfg.scale_penalty
+        teacher_responses[:self.cfg.scale_num // 2] *= self.cfg.scale_penalty
+        teacher_responses[self.cfg.scale_num // 2 + 1:] *= self.cfg.scale_penalty
+        # peak scale
+        scale_id = np.argmax(np.amax(responses, axis=(1, 2)))
+        teacher_scale_id = np.argmax(np.amax(teacher_responses, axis=(1, 2)))
+        # peak location
+        response = responses[scale_id]
+        teacher_response = teacher_responses[teacher_scale_id]
+        response -= response.min()
+        response /= response.sum() + 1e-16
+        teacher_response -= teacher_response.min()
+        teacher_response /= teacher_response.sum() + 1e-16
+        response = (1 - self.cfg.window_influence) * response + \
+            self.cfg.window_influence * self.hann_window
+        teacher_response = (1 - self.cfg.window_influence) * teacher_response + \
+            self.cfg.window_influence * self.h
+        loc = np.unravel_index(response.argmax(), response.shape)
+        teacher_loc = np.unravel_index(teacher_response.argmax(), teacher_response.shape)
+        # locate target center
+        disp_in_response = np.array(loc) - (self.upscale_sz - 1) / 2
+        teacher_disp_in_response = np.array(teacher_loc) - (self.upscale_sz - 1) / 2
+        disp_in_instance = disp_in_response * \
+            self.cfg.total_stride / self.cfg.response_up
+        teacher_disp_in_instance = teacher_disp_in_response * \
+            self.cfg.total_stride / self.cfg.response_up
+        disp_in_image = disp_in_instance * self.x_sz * \
+            self.scale_factors[scale_id] / self.cfg.instance_sz
+        teacher_disp_in_image = teacher_disp_in_instance * self.x_sz * \
+            self.scale_factors[teacher_scale_id] / self.cfg.instance_sz
+        self.center += disp_in_image
+        teacher_center += teacher_disp_in_image
+        # update target size
+        scale =  (1 - self.cfg.scale_lr) * 1.0 + \
+            self.cfg.scale_lr * self.scale_factors[scale_id]
+        teacher_scale =  (1 - self.cfg.scale_lr) * 1.0 + \
+            self.cfg.scale_lr * self.scale_factors[teacher_scale_id]
+        self.target_sz *= scale
+        teacher_target_sz *= teacher_scale
+        self.z_sz *= scale
+        teacher_z_sz *= teacher_scale
+        self.x_sz *= scale
+        teacher_x_sz *= teacher_scale
+        # return 1-indexed and left-top based bounding box
+        box = np.array([
+            self.center[1] + 1 - (self.target_sz[1] - 1) / 2,
+            self.center[0] + 1 - (self.target_sz[0] - 1) / 2,
+            self.target_sz[1], self.target_sz[0]])
+        teacher_box = np.array([
+            teacher_center[1] + 1 - (teacher_target_sz[1] - 1) / 2,
+            teacher_center[0] + 1 - (teacher_target_sz[0] - 1) / 2,
+            teacher_target_sz[1], teacher_target_sz[0]])
+        return box, teacher_box
