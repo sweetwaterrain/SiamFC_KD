07ef3f174425bafed6936e12d5860057b4938ca2
gly <993214226@qq.com>
2023-07-01 20:05:25 +0800

    修改backbone

--- siamfc/backboneVanillaNet.py
+++ siamfc/backboneVanillaNet.py
@@ -3,19 +3,16 @@
 from .VanillaNet.models.vanillanet import vanillanet_5
 import torch.nn as nn
 
+# vanillanet_5 = vanillanet_5()
+# #修改网络最后一层的输出通道数，使得输出通道数为256，与SiamFC网络的特征通道数一致，方便后续的特征提取，同时保证网络的输出尺寸不变
+# vanillanet_5.conv5 = nn.Conv2d(128, 256, 3, 1, groups=2)
+
 class VanillaNet(nn.Module):
     def __init__(self):
         super(VanillaNet, self).__init__()
-        self.conv1 = vanillanet_5()
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(256, 256, 3, 1, groups=2),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True))
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(256, 256, 3, 1, groups=2))
+        self.features = vanillanet_5()
+        #修改网络最后一层的输出通道数，使得输出通道数为256，与SiamFC网络的特征通道数一致，方便后续的特征提取，同时保证网络的输出尺寸不变
+        self.features.conv5 = nn.Conv2d(128, 256, 3, 1, groups=2)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        return x
+        return self.features(x)
