355e8ec610f9319775123ec5b8621c07e0ebea23
gly <993214226@qq.com>
2023-07-01 19:14:36 +0800

    修改train

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     # seqs = GOT10k(root_dir, subset='train', return_meta=True)
 
     root_dir = os.path.expanduser('/kaggle/input/otb2015/OTB100')
-    seqs = OTB(root_dir, version=2015, return_meta=True)
+    seqs = OTB(root_dir, version=2015)
 
     tracker = TrackerSiamFC()
     tracker.train_over(seqs)
